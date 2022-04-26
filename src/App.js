4c4e9eed3861611f5e7aedc322441462b46ce974
Paulo Henrique Reis <87020059+paulooliveirv@users.noreply.github.com>
2022-04-25 21:46:28 -0300

    adicionando funcionalidades botoes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [resultado, mostraResultado] = useState("");
+
+    const clique = (e) => {
+        mostraResultado(resultado.concat(e.target.name));
+    }
+
+    const limpa = () => {
+        mostraResultado("");
+    }
+
+    const apagaItem = () => {
+        mostraResultado(resultado.slice(0, resultado.length -1));
+    }
+    return (
+        <div className = "container">
+            <form>
+                <input type="text" value= {resultado}/>
+            </form>
+
+            <div className="teclado">
+                <button className="botoesEspeciais" onClick={ limpa } id="limpa">Limpa</button>
+                <button className="botoesEspeciais" onClick={ apagaItem } id="c">C</button>
+                <button className="botoesEspeciais" name= "/" onClick={ clique }>&divide;</button>
+                <button name= "7" onClick={ clique }>7</button>
+                <button name= "8" onClick={ clique }>8</button>
+                <button name= "9" onClick={ clique }>9</button>
+                <button className="botoesEspeciais" name= "*" onClick={ clique }>&times;</button>
+                <button name= "4" onClick={ clique }>4</button>
+                <button name= "5" onClick={ clique }>5</button>
+                <button name= "6" onClick={ clique }>6</button>
+                <button className="botoesEspeciais" name= "-" onClick={ clique }>&ndash;</button>
+                <button name= "1" onClick={ clique }>1</button>
+                <button name= "2" onClick={ clique }>2</button>
+                <button name= "3" onClick={ clique }>3</button>
+                <button className="botoesEspeciais" name= "+" onClick={ clique }>+</button>
+                <button className = "botaoZero" name= "0" onClick={ clique }>0</button>
+                <button className="botoesEspeciais" name= "." onClick={ clique }>.</button>
+                <button className="botoesEspeciais" onClick={ clique } id= "resultado">=</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
